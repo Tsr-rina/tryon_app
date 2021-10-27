3f6b991367ea7c1f27c5c948d8c3597af0c47e31
rinas <topi2frie3elec@gmail.com>
2021-10-27 09:57:06 +0900

    make knsu

--- src/django_app/views.py
+++ src/django_app/views.py
@@ -10,6 +10,7 @@ import io
 import base64
 from django_app.senga import makecounter
 from django_app.overlay import overlayImage
+from django_app.image_processing import gan_preprocessing, to_white, synthetic, opencv_to_pil
 
 def index(request):
     return render(request, 'index.html')
@@ -164,119 +165,29 @@ def try_on(request):
             hm_path = list(list(hm_img)[0].values())
             hm_path = "./media/"+str(hm_path[0])
 
-            # ここに画像処理
-            mask_img = cv2.imread(mask_path)
-            print("mask_img.shape:{}".format(mask_img.shape))
-            resized_mask = cv2.resize(mask_img, dsize=(192,256))
-            print("resized_mask:{}".format(resized_mask.shape))
+            # gan_preprocessing→入力画像生成
+            colored_image, get_mask_shape = gan_preprocessing(mask_path, cloth_path)
+            print("colored_image:{}".format(colored_image.shape))
 
-            # 画像抽出paste
-            get_mask = resized_mask[45:128, 57:130]
-            get_mask_shape = get_mask.shape
-            print("get_mask:{}".format(get_mask.shape))
-            get_mask = cv2.resize(get_mask, dsize=(192,256))
-            print("抽出後のリサイズ:{}".format(get_mask.shape))
-
-            # 洋服の線画past
-            cloth = makecounter(cloth_path)
-            # cloth = cv2.imread()
-            print("cloth.shape:{}".format(cloth.shape))
-            resized_cloth = cv2.resize(cloth, dsize=(192,256))
-            print("resized_cloth:{}".format(resized_cloth.shape))
-
-            # オーバーレイ
-            black = [0, 0, 0]
-            hotpink = [255, 105, 180]
-            get_mask[np.where((get_mask == black).all(axis=2))] = hotpink
-            get_mask = cv2.cvtColor(get_mask, cv2.COLOR_RGB2BGRA)
-            get_mask[:, :,3] = np.where(np.all(get_mask == (255, 255,255,255), axis=-1), 0, 255)  # 白色のみTrueを返し、Alphaを0にする
-            colored_image = overlayImage(cloth, get_mask, (0, 0))   
-
-            # GANへ
-            # 紫色をしろ色にする処理を書く
+            # 紫色を白色にする処理を書く
+            # to_whited = to_white(colored_image)
+            # GAN→to_whiteを持っていくだけでOK,png形式で送る？
+            # image_make = CallNetWork(str(img_path), str(mask_path), str(cloth_path))
 
             # GANの結果を受け取り
-            # なのでcolored_imageが変更の可能性あり
-            made = cv2.resize(colored_image, dsize=(get_mask_shape[1], get_mask_shape[0]))
-            made = cv2.cvtColor(made, cv2.COLOR_BGRA2BGR)
-            print("made.shape:{}".format(made.shape))
-            mask = cv2.imread(mask_path)
-            mask = cv2.resize(mask, dsize=(192, 256))
-            print("mask.shape:{}".format(mask.shape))
-            made_plot = [[0,0],[0,get_mask_shape[0]],[get_mask_shape[1],get_mask_shape[0]], [get_mask_shape[1],0]]
-            mask_plot = [[57,45],[57,128],[130, 128], [130, 45]]
-
-            # バウンディングボックスで切り出し
-            src_pts_arr = np.array(made_plot, dtype=np.float32)
-            dst_pts_arr = np.array(mask_plot, dtype=np.float32)
-
-            src_rect = cv2.boundingRect(src_pts_arr)
-            dst_rect = cv2.boundingRect(dst_pts_arr)
-
-            print("src_rect:{}".format(src_rect))
-            print("dst_rect:{}".format(dst_rect))
-
-
-            made_crop = made[src_rect[1]:src_rect[1] + src_rect[3], src_rect[0]:src_rect[0] + src_rect[2]]
-            mask_crop = mask[dst_rect[1]:dst_rect[1] + dst_rect[3]-1, dst_rect[0]:dst_rect[0] + dst_rect[2]-1]
-            print(made_crop.shape)
-            print(mask_crop.shape)
-            made_pts_crop = src_pts_arr - src_rect[:2]
-            mask_pts_crop = dst_pts_arr - dst_rect[:2]
-
-            print("made_crop:{}".format(made_pts_crop))
-            print("mask_crop:{}".format(mask_pts_crop))
-
-            masker = np.zeros_like(mask_crop, dtype=np.float32)
-            cv2.fillConvexPoly(masker, mask_pts_crop.astype(np.int), (1.0,1.0,1.0), cv2.LINE_AA)
-            dst_crop_merge = made_crop * mask_crop + mask_crop * (1 - mask_crop)
-
-            mask[dst_rect[1]:dst_rect[1]+dst_rect[3]-1, dst_rect[0]:dst_rect[0]+dst_rect[2]-1] = dst_crop_merge
-
-            # GAN済のマスク画像が生成できたら人体モデルと合成する
-            mask = cv2.resize(mask, dsize=(192,256))
-            black = [0, 0, 0]
-            hotpink = [255, 105, 180]
-            mask[np.where((mask == black).all(axis=2))] = hotpink
-            mask = cv2.cvtColor(mask, cv2.COLOR_RGB2BGRA)
-            mask[:, :,3] = np.where(np.all(mask == (180,105,255,255), axis=-1), 0, 255)  # 白色のみTrueを返し、Alphaを0にする
-            # 人体モデル読み込み
-            hm = cv2.imread(hm_path)
-            hmed = cv2.resize(hm, dsize=(192,256))
-
-            all_got = overlayImage(hmed, mask, (0, 0))
+            # なのでcolored_imageが変更の可能性あり(image_makeかな)
+            # 値を受け渡す
+            # all_got_img = synthetic(to_whited,mask_path,hm_path)
+            all_got_img = synthetic(get_mask_shape, colored_image,mask_path,hm_path)
 
             # OpenCV→PIL変換
-            image_cv = cv2.cvtColor(all_got, cv2.COLOR_BGR2RGB)
-            image_pil = Image.fromarray(image_cv)
-            image_pil = image_pil.convert('RGB')
-
-            # 画像をhtmlに持っていく準備
-            buffer = io.BytesIO()
-            image_pil.save(buffer, format="PNG")
-
-            base64Img = base64.b64encode(buffer.getvalue()).decode().replace("'","")
-
-            # 人体モデル
-            # hm_img = Human_model_img.objects.values('img').filter(model_name=keys_list[0])
-            # img_path = list(list(hm_img)[0].values())[0]
-
-            # # マスク画像
-            # hm_mask = Human_model_img.objects.values('mask').filter(model_name=keys_list[0])
-            # mask_path = list(list(hm_mask)[0].values())[0]
-
-            # print("ganmodule行ってきます")
-            # image_make = CallNetWork(str(img_path), str(mask_path), str(cloth_path))
-            # made = image_make.forward()
-            # print("戻ってきました")
-            # print(made.shape)
-
+            transfer_img = opencv_to_pil(all_got_img)
 
             context = {
                 'hello': 'Hello World!',
                 'cloth_path':'media/cloth_img/cloth1.jpg',
                 'mask_path':mask_path,
-                'base64Img':base64Img,
+                'transfer_img':transfer_img,
             }
             return render(request, 'result.html', context)
 
