0cf5b6fb9b5330f46e07da0a7a9f0ea5f68cd617
rinas <topi2frie3elec@gmail.com>
2021-11-01 20:02:16 +0900

    add gcp storage

---
+++ src/django_prj/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['tryonapps.herokuapp.com']
+
